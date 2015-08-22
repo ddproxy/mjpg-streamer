74a9f8a1ffb6f056a22733691c643b834195242c
moosd <souramoo@gmx.com>
2015-08-22 15:54:17 +0000

    Changed run parameters...

--- mjpg-streamer-experimental/run.sh
+++ mjpg-streamer-experimental/run.sh
@@ -1,2 +1,2 @@
 export LD_LIBRARY_PATH=.
-./mjpg_streamer -o "./output_http.so --port 9002 -w ./www" -i "input_raspicam.so -fps 24 -x 800 -y 600"
+./mjpg_streamer -o "./output_http.so --port 9002 -w ./www" -i "input_raspicam.so -fps 20 -x 800 -y 600"
