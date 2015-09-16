05d0a6f77f1302c4e8de43613ef2d85a4e1b9ed7
moosd <souramoo@gmx.com>
2015-09-16 00:16:05 +0000

    Ability to modify ISO

--- mjpg-streamer-experimental/run.sh
+++ mjpg-streamer-experimental/run.sh
@@ -1,2 +1,7 @@
 export LD_LIBRARY_PATH=.
-./mjpg_streamer -o "./output_http.so --port 9002 -w ./www" -i "input_raspicam.so -fps 20 -x 800 -y 600"
+iso=400
+if [ ! -z "$1" ]; then
+    iso="$1"
+fi
+
+./mjpg_streamer -o "./output_http.so --port 9002 -w ./www" -i "input_raspicam.so -fps 20 -x 800 -y 600 -ISO $iso"
